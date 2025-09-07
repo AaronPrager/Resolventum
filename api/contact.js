138934e33cfecb0dbfa90046f2e4bea09b2ecacc
Aaron Prager <arik@pragersfamily.com>
2025-09-07 19:13:18 -0400

    Update contact form

--- api/contact.js
+++ api/contact.js
@@ -14,7 +14,7 @@ module.exports = async function handler(req, res) {
     }
 
     // Create transporter using Gmail SMTP
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER, // Your Gmail address
